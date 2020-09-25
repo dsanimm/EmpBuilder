538c2e337bb6760a082b52d76293f89b32a1a3d4
dsanimm <dsanimm@gmail.com>
2020-09-25 17:22:07 +0530

    UC4

--- javaproject/src/javaproject/EmpWageBuilder.java
+++ javaproject/src/javaproject/EmpWageBuilder.java
@@ -1,26 +1,35 @@
 package javaproject;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
 
 public class EmpWageBuilder {
 public static final int IS_PART_TIME=1;
 public static final int IS_FULL_TIME=2;
+private int numOfCompany=0;
+private LinkedList<CompanyEmpWage> companyEmpWageList;
+private Map<String,CompanyEmpWage> companyToEmpWageMap;
+public EmpWageBuilder() {
+	companyEmpWageList = new LinkedList<>();
+	companyToEmpWageMap=new HashMap<>();
+}
+public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+	CompanyEmpWage companyEmpWage = new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
+companyEmpWageList.add(companyEmpWage);
+companyToEmpWageMap.put(company, companyEmpWage);
 
-	public final String company;
-	public final int empRatePerHour;
-	public final int numOfWorkingDays;
-	public final int maxHoursPerMonth;
-	public int totalEmpWage;
-public EmpWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-{
-	this.company=company;
-	this.empRatePerHour=empRatePerHour;
-	this.numOfWorkingDays=numOfWorkingDays;
-	this.maxHoursPerMonth=maxHoursPerMonth;
+}
 	
+public void computeEmpWage() {	
+	for(int i=0;i<companyEmpWageList.size();i++) {
+		CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+		companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+		System.out.println(companyEmpWage);
 	}
-
-
-public void computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+}
+public int computeEmpWage(CompanyEmpWage companyEmpWage) {
 	int empHrs =0,totalEmpHrs=0,totalWorkingDays=0;
 	while(totalEmpHrs <= companyEmpWage.maxHoursPerMonth &&
 		    totalWorkingDays < companyEmpWage.numOfWorkingDays) {
@@ -41,23 +50,58 @@ public void computeEmpWage(String company,int empRatePerHour,int numOfWorkingDay
 			System.out.println("Day#: " + totalWorkingDays + " EMpe Hr: " +empHrs);
 			//System.out.println("Emp Wage: " +empWage);
 		}
-int totalEmpWage =  totalEmpHrs *empRatePerHour;
-//System.out.println(Total Emp Wage for Company: "+company+" is: "+totalEmpWage  );
+return totalEmpHrs *companyEmpWage.empRatePerHour;
 		
-
-
 }
-
-public String toString(){
-System.out.println(Total Emp Wage for Company: "+company+" is: "+totalEmpWage  );
+public int getTotalWage(String company) {
+	return companyToEmpWageMap.get(company).totalEmpWage;
 }
 public static void main(String[] args) {
-EmpWageBuilder dMart=new EmpWageBuilder("DMart",20,2,10);
-EmpWageBuilder dMart=new EmpWageBuilder("Reliance",10,4,20);
- dMrt.computeEmpWage();
-System.out.println(dMart);
-reliance.computeWage();
-System.out.println(reliance);
+	EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+
+String company;
+int empRatePerHour, numOfWorkingDays,maxHoursPerMonth;
+Scanner sc=new Scanner(System.in);
+int choice =1;
+while(choice!=4) {
+System.out.println("Enter your choice:\n1. Enter Company Details\n2. Compute Total Wage for chosen company\n3. Compute Total Wage for all companies\n4. Exit");
+
+	choice = sc.nextInt();
+
+
+	switch(choice) {
+	case 1:
+		System.out.println("Enter company:");
+		company = sc.next();
+		System.out.println("Enter empRatePerHour:");
+		empRatePerHour=sc.nextInt();
+		System.out.println("Enter numOfWorkingDays:");
+		numOfWorkingDays=sc.nextInt();
+		System.out.println("Enter maxHoursPerMonth:");
+		maxHoursPerMonth=sc.nextInt();
+		System.out.println("Record Added");
+		empWageBuilder.addCompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		System.out.println("Company added");
+		break;
+	case 2:
+		
+		System.out.println("Enter the company name");
+		company = sc.next();
+		System.out.println("Total Wage for "+company+" "+ empWageBuilder.getTotalWage(company));
+		
+		break;
+	
+	case 3:
+		empWageBuilder.computeEmpWage();
+		break;
+	case 4:
+		break;
+	default: System.out.println("Wrong choice");
+		
+		
+
+}
+}
 
 }
 
