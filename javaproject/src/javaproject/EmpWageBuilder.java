cdeea91023dd9dc5855994c6fde5b6f53d687d58
dsanimm <dsanimm@gmail.com>
2020-09-25 17:14:15 +0530

    firstcommit

---
+++ javaproject/src/javaproject/EmpWageBuilder.java
@@ -0,0 +1,16 @@
+package javaproject;
+
+public class EmpWageBuilder
+{
+    public static void main( String[] args )
+    {
+        
+
+    	System.out.println( "Hello World!" );
+      
+
+        
+        
+    }
+}
+
